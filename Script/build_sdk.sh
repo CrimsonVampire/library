b99dd465fceb98cc96dbceccdae84d0ec72b2919
xiaorui.wang <xiaorui.wang>
2022-07-01 16:42:43 +0800

    buld.sh commit

--- Script/build_sdk.sh
+++ Script/build_sdk.sh
@@ -1,50 +1,54 @@
 #!/bin/sh
 
-#  build_sdk.sh
+echo "############ My iOS SDK Build 2016 ############"
 
-sh_root_dir=$(cd `dirname $0`; pwd)
+test -n "$XCODEBUILD"   || XCODEBUILD=$(which xcodebuild)
+test -n "$LIPO"         || LIPO=$(which lipo)
+test -n "$PRODUCTSIGN"  || PRODUCTSIGN=$(which productsign)
+test -n "$PLISTBUDDY"   || PLISTBUDDY=/usr/libexec/PlistBuddy
 
-build_lib_dir=$sh_root_dir/build_lib.sh
-build_framework_dir=$sh_root_dir/build_framework.sh
-build_copyfiles_dir=$sh_root_dir/build_copyfiles.sh
-
-output_root_dir=$sh_root_dir/../../../Release/iOS
-output_root_core_dir=$output_root_dir/Core/Base
-
-chmod 777 $build_lib_dir
-chmod 777 $build_framework_dir
-
-base
-sh $build_lib_dir "Plugins/Base/FunBase" "FunBase" $output_root_core_dir/FunBase
-sh $build_lib_dir "Plugins/Base/FunBaseUI" "FunBaseUI" $output_root_core_dir/FunBase
-sh $build_lib_dir "Plugins/Base/FunCatch" "FunCatch" $output_root_core_dir/FunBase
-sh $build_lib_dir "Plugins/Base/FunLogAgent" "FunLogAgent" $output_root_core_dir/FunBase
-
-sh $build_lib_dir "Plugins/H5/FunH5SDK" "FunH5SDK" $output_root_dir/FPXCMSImp/res/libs
-sh $build_framework_dir "FPSDK/CMS/FunCMS/FunCMS" "FunCMS" $output_root_dir/FPXCMSImp/res/libs
-
-if [[ ! -d $output_root_dir/FPXCMSImp/res/res ]]; then
-    mkdir $output_root_dir/FPXCMSImp/res/res
+# < XCode 4.3.1
+if [ ! -x "$XCODEBUILD" ]; then
+# XCode from app store
+XCODEBUILD=/Applications/XCode.app/Contents/Developer/usr/bin/xcodebuild
 fi
-cp -R $sh_root_dir/../../SDK/iOS-SDK/FPSDK/CMS/FunCmsDemo/FunCmsDemo/FP_CMSRes.bundle $output_root_dir/FPXCMSImp/res/res
 
-sh $build_lib_dir "FPSDK/Notice/FunNotice" "FunNotice" $output_root_dir/FPXNotice/res/libs
+test -x "$XCODEBUILD" || die 'Could not find xcodebuild in $PATH'
 
-sh $build_lib_dir "FPSDK/Privacy/FunPrivacy" "FunPrivacy" $output_root_dir/FPXPrivacy/res/libs
-sh $build_lib_dir "FPSDK/Privacy/FunPrivacyUI" "FunPrivacyUI" $output_root_dir/FPXPrivacy/res/libs
+echo $XCODEBUILD  #get xcodebuild path
+echo $LIPO
+echo $PRODUCTSIGN
+echo $PLISTBUDDY
 
-sh $build_lib_dir "FPSDK/Push/FunPush" "FunPush" $output_root_dir/FPXPush/res/libs
+# ----------------------------------------------------------------------------
+# Set up pathes
+#
+SDK_VERSION="1.0.0"
+echo "############ My iOS SDK Build Version $SDK_VERSION ############"
 
-sh $build_lib_dir "FPSDK/Attribution/FunAttribution" "FunAttribution" $output_root_dir/FPXAdvert/FPXAdvertFunImp/res/libs
+KG_sdk_script=$(pwd)
+KG_sdk_root=$(dirname $KG_sdk_script)
 
-sh $build_lib_dir "FPSDK/Payment/FunAppleIAP" "FunAppleIAP" $output_root_dir/FPXIMP/res/libs
-sh $build_lib_dir "FPSDK/Account/Account/FPAccount" "FPAccount" $output_root_dir/FPXIMP/res/libs
-sh $build_lib_dir "FPSDK/Account/Account/FPAccountBase" "FPAccountBase" $output_root_dir/FPXIMP/res/libs
-sh $build_lib_dir "FPSDK/Account/AccountUI/FPAccountUI" "FPAccountUI" $output_root_dir/FPXIMP/res/libs
-sh $build_copyfiles_dir "FPXThirdLibs/ShuMei" "FPXIMP/res/libs"
+echo $KG_sdk_script
+echo $KG_sdk_root
 
-if [[ ! -d $output_root_dir/FPXIMP/res/res ]]; then
-    mkdir $output_root_dir/FPXIMP/res/res
+export KG_sdk_folder_name="MY-iOS-SDK"
+echo $KG_sdk_folder_name
+
+export KG_sdk_zip_folder_name=$KG_sdk_folder_name
+echo $KG_sdk_zip_folder_name
+
+rm -rf "${KG_sdk_root}/release/"
+mkdir "${KG_sdk_root}/release/"
+
+KG_sdk_release="${KG_sdk_root}/release/${KG_sdk_folder_name}"
+
+echo $KG_sdk_release
+
+
+if [ -d $KG_sdk_release ];then
+    echo "[Error]Version ${SDK_VERSION} already exist: ${KG_sdk_release}"
+    exit 1
+else
+    mkdir $KG_sdk_release
 fi
-cp -R $sh_root_dir/../../SDK/iOS-SDK/FPSDK/Account/AccountDemo/AccountDemo/FPAccountSdk.bundle $output_root_dir/FPXIMP/res/res
-
